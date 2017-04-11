d3d79c654f2fa4a57f4fc7443a433b483b883d78
yijaejun <yijaejun81@gmail.com>
2017-04-11 14:32:18 +0900

    no message

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,12 +12,12 @@ require.config({
 		// lodash : ['/components/lodash/dist/lodash.min'],
 		// fastclick : ['/components/fastclick/lib/fastclick'],
 		// swiper : ['/components/swiper/dist/js/swiper.jquery'],
-		common : ['/javascripts/common'],
-		vue : ['/components/vue/dist/vue'],
+		common : ['/static/javascripts/common'],
+		vue : ['/static//javascripts/vendor/vue.min'],
 		//vue : ['https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.6/vue.min'],
 		//videojs : ['http://vjs.zencdn.net/5.19.0/video'],
-		videojs : ['/javascripts/vendor/videojs'],
-		youtubejs : ['/static/javascripts/youtube.min']
+		videojs : ['/static/javascripts/vendor/videojs'],
+		youtubejs : ['/static/javascripts/vendor/youtube.min']
 	},
 	shim: {
 		jqueryValidate : ['jquery'],
