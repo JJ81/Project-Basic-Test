d240ab71a85bc820dfef0af1ff2f1a7916ece60c
yijaejun <yijaejun81@gmail.com>
2017-04-28 16:11:02 +0900

    no message

--- public/javascripts/m_nav.js
+++ public/javascripts/m_nav.js
@@ -4,8 +4,8 @@ const
 	_right_nav = $('.btn-nav-list'),
   _left_nav = $('.btn-channel-list'),
  _right_nav_body = $('.right_menu_m'),
- _left_nav_body = $('.left_menu_m');
-
+ _left_nav_body = $('.left_menu_m'),
+ _body_content = $('.body_content');
 
 _right_nav.bind('click', (e) => {
 	e.preventDefault();
@@ -17,24 +17,32 @@ _right_nav.bind('click', (e) => {
 
 	if(_right_nav_body.hasClass('blind')){
 		_right_nav_body.removeClass('blind');
+		_body_content.addClass('blind');
 	}else{
 		_right_nav_body.addClass('blind');
+		_body_content.removeClass('blind');
 	}
 
 	return;
 });
 
-
 // hide all menu
-$('body').bind('click', () => {
-	if(!_right_nav_body.hasClass('blind')){
-		_right_nav_body.addClass('blind');
-	}
-
-	if(!_left_nav_body.hasClass('blind')){
-		_left_nav_body.addClass('blind');
-	}
-});
+// $('document').bind('click', () => {
+// 	if(!_right_nav_body.hasClass('blind')){
+// 		_right_nav_body.addClass('blind');
+// 	}
+//
+// 	if(!_left_nav_body.hasClass('blind')){
+// 		_left_nav_body.addClass('blind');
+// 	}
+//
+// 	if(_body_content.hasClass('blind')) {
+// 		_body_content.removeClass('blind');
+// 	}
+//
+// 	alert('body or document');
+//
+// });
 
 
 _left_nav.bind('click', (e) => {
@@ -47,8 +55,10 @@ _left_nav.bind('click', (e) => {
 
 	if(_left_nav_body.hasClass('blind')){
 		_left_nav_body.removeClass('blind');
+		_body_content.addClass('blind');
 	}else{
 		_left_nav_body.addClass('blind');
+		_body_content.removeClass('blind');
 	}
 
 	return;
