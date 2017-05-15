cfda38514e2231859c7089e6ffed9e75afb2d64a
yijaejun <yijaejun81@gmail.com>
2017-05-15 17:40:46 +0900

    change game download link

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ const isMobile = require('is-mobile');
 app.use((req, res, next) => {
 	res.locals.version  = '2.0.0';
 	res.locals.isMobile = (isMobile(req) == 1) ? 1 : 0;
-	res.locals.download = 'javascript:alert("준비중입니다.")';
+	res.locals.download = 'https://play.google.com/store/apps/details?id=com.hc.holdem.live';
 
 	if(req.headers['x-forwarded-port'] === '443'){
 		res.locals.static = 'https://assets.holdemclub.tv/';
