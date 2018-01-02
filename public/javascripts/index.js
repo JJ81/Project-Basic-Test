b7aeb8e2b908968616b49e88c065f7664fac0e51
yijaejun <yijaejun81@gmail.com>
2018-01-02 12:53:28 +0900

    VIP회원수 옆에 ticker 적용함.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,5 +11,25 @@
 		autoplay: 6000,
 		autoplayDisableOnInteraction: false
 	});
+
+
+	$(function(){
+		var timer = !1;
+		_Ticker = $("#T1").newsTicker();
+		_Ticker.on("mouseenter",function(){
+			var __self = this;
+			timer = setTimeout(function(){
+				__self.pauseTicker();
+			}, 300);
+		});
+		_Ticker.on("mouseleave",function(){
+			clearTimeout(timer);
+			if(!timer) return !1;
+			this.startTicker();
+		});
+	});
+
 }());
+
+
 
