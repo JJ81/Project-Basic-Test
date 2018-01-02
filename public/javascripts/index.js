5bf5b45448edcc7a1c04a97eb6205cc265f97b15
yijaejun <yijaejun81@gmail.com>
2018-01-02 11:41:12 +0900

    방송중 아이콘 적용 및 상단 swiper 적용

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,15 @@
-'use strict';
-import $ from '../components/jquery/dist/jquery.min';
-import ps from './vendor/ps.estate';
-import util from './utils';
-import Swiper from './vendor/swiper.min';
+(function () {
+	new Swiper('.swiper-container', {
+		pagination: '.swiper-pagination',
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		slidesPerView: 1,
+		paginationClickable: true,
+		spaceBetween: 0,
+		loop: true,
+		centeredSlides: true,
+		autoplay: 6000,
+		autoplayDisableOnInteraction: false
+	});
+}());
 
-
-new Swiper('.swiper-container', {
-	pagination: '.swiper-pagination',
-	nextButton: '.swiper-button-next',
-	prevButton: '.swiper-button-prev',
-	slidesPerView: 1,
-	paginationClickable: true,
-	spaceBetween: 0,
-	loop: true,
-	centeredSlides: true,
-	autoplay: 6000,
-	autoplayDisableOnInteraction: false
-});
